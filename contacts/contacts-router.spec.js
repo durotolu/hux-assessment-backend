9fc97ab371ffd24aabe508a8a2935cd94966ed2e
durotolu <durolawk@gmail.com>
2024-05-05 11:07:18 +0100

    added more contacts tests

--- contacts/contacts-router.spec.js
+++ contacts/contacts-router.spec.js
@@ -20,14 +20,14 @@ jest.mock("./contacts-model.js", () => ({
   add: jest.fn(),
 }));
 
+const mockContactData = {
+  firstname: "John",
+  lastname: "Doe",
+  phone_number: "12345",
+};
+
 describe("POST add new contact", () => {
   test("should return 201 and the contact data when request is valid", async () => {
-    const mockContactData = {
-      firstname: "John",
-      lastname: "Doe",
-      phone_number: "12345",
-    };
-
     contactsModel.add.mockResolvedValue(mockContactData);
 
     const response = await request(server)
@@ -36,5 +36,54 @@ describe("POST add new contact", () => {
       .expect(201);
 
     expect(response.body).toEqual(mockContactData);
+  });
+
+  test("should return 500 and an error message when Contacts.add throws an error", async () => {
+    const errorMessage = "Internal Server Error";
+    contactsModel.add.mockRejectedValue(new Error(errorMessage));
+
+    const response = await request(server)
+      .post("/api/contacts")
+      .send(mockContactData)
+      .expect(500);
+
+    expect(response.text).toContain(errorMessage);
+  });
+});
+
+describe("PUT update existing contact", () => {
+  test("should return 201 and the updated contact data when request is valid", async () => {
+    const mockContactId = 1;
+    const mockUpdatedContactData = {
+      firstname: "Updated Firstname",
+      lastname: "Updated Lastname",
+      phone_number: "54321",
+    };
+    contactsModel.update = jest.fn().mockResolvedValue(mockUpdatedContactData);
+
+    const response = await request(server)
+      .put(`/api/contacts/${mockContactId}`)
+      .send(mockUpdatedContactData)
+      .expect(201);
+
+    expect(response.body).toEqual(mockUpdatedContactData);
+  });
+
+  test("should return 500 and error message when update operation fails", async () => {
+    const mockContactId = 1;
+    const mockUpdatedContactData = {
+      firstname: "Updated Firstname",
+      lastname: "Updated Lastname",
+      phone_number: "54321",
+    };
+    const errorMessage = "Update operation failed";
+    contactsModel.update = jest.fn().mockRejectedValue(new Error(errorMessage));
+
+    const response = await request(server)
+      .put(`/api/contacts/${mockContactId}`)
+      .send(mockUpdatedContactData)
+      .expect(500);
+
+    expect(response.body).toEqual({ "error updating contact": errorMessage });
   });
 });
